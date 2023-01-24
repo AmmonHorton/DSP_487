8b4be72f3983c9ff2587fa46fd36878ed4a1fe1c
AJ_Horton <aj_horton@hotmail.com>
2023-01-23 17:17:27 -0700

    lab1 matlab files

--- lab1/fir_bandpass.m
+++ lab1/fir_bandpass.m
@@ -1,13 +1,14 @@
+clear all;
+
 % FIR Bandpass Filter
-
 centerFreq = 3e3;
-firWinLength = 81;
-bandwidth = 600;
+firWinLength = 101;
+bandwidth = 400;
 
-fsample = 18e3;
+fsample = 16200;
 
 % Create the bandpass filter by making two low pass filters and doing a spectral inversion the latter
-N = firWinLength/4;
+N = floor(firWinLength/4);
 n = (-N:N);
 %n = n - floor(N/2)
 lpcoef = 2 * (centerFreq+bandwidth);
@@ -21,14 +22,31 @@ hpfir = hpcoef * sinc(hpcoef*n);
 
 % Spectral Reversal
 hpfir(1:2:end) = -hpfir(1:2:end);
+
+
+
 % Convolve the two to create bandpass filter
 bpfir = conv(hpfir,lpfir);
 figure();
 freqz(bpfir);
 
-
-bpfir = bpfir .* hamming(firWinLength).';
-length(bpfir)
+bpfir = bpfir .* blackman(firWinLength).';
+% bpfir = bpfir .* hamming(firWinLength).';
 
 figure();
 freqz(bpfir);
+
+
+[y, Fs] = audioread('mix10 2021.wav');
+
+y_filtered = conv(y,bpfir);
+% sound(y_filtered, Fs);
+
+
+y_filtered = y_filtered.^2;
+axis = 1:length(y_filtered);
+
+figure();
+plot(axis,y_filtered);
+
+
